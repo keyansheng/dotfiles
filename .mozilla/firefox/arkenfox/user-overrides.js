3080e45ef864237d5584dea041fc506994c0fb57
keyansheng <65121402+keyansheng@users.noreply.github.com>
2024-03-10 11:19:09 +0800

    add arkenfox

---
+++ .mozilla/firefox/arkenfox/user-overrides.js
@@ -0,0 +1,11 @@
+user_pref("browser.safebrowsing.downloads.remote.enabled", true);
+user_pref("privacy.resistFingerprinting", false);
+user_pref("privacy.resistFingerprinting.letterboxing", false);
+user_pref("webgl.disabled", false);
+
+user_pref("browser.uidensity", 1);
+user_pref("extensions.pocket.enabled", false);
+
+user_pref("browser.bookmarks.editDialog.showForNewBookmarks", false);
+user_pref("browser.fullscreen.exit_on_escape", false);
+user_pref("findbar.highlightAll", true);
